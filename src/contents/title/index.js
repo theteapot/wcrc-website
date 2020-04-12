8b2691b1935a2703befa1efed4b58907573a6a31
Taylor Kettle <tin.teapot@gmail.com>
2020-04-12 17:58:34 +1200

    changed subtitle

--- src/contents/title/index.js
+++ src/contents/title/index.js
@@ -8,7 +8,9 @@ export default class Title extends Component {
         <div className="title">Whangaparoa Community Recycling Centre</div>
         <div className="subtitle">
           Owned by Auckland Council, Managed by Hibiscus Coast Zero Waste
-          Working together towards zero waste
+          <div>
+            <i>Working together towards zero waste</i>
+          </div>
         </div>
       </div>
     );
